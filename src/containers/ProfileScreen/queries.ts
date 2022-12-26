de2a04bf1ffd33bee5d987728569744dc15f56a8
Masood97 <masoodchohan97@gmail.com>
2022-12-26 07:51:16 +0500

    userfollow and unfollow

--- src/containers/ProfileScreen/queries.ts
+++ src/containers/ProfileScreen/queries.ts
@@ -32,3 +32,55 @@ export const getUser = gql `
     }
   }
 `;
+export const createUserFollow = gql`
+  mutation CreateUserFollow(
+    $input: CreateUserFollowInput!
+    $condition: ModelUserFollowConditionInput
+  ) {
+    createUserFollow(input: $input, condition: $condition) {
+      id
+      followerID
+      followeeID
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      owner
+    }
+  }
+`;
+
+export const userFollowings = gql `
+  query UserFollowings(
+    $followerID: ID!
+    $followeeID: ModelIDKeyConditionInput
+    $sortDirection: ModelSortDirection
+    $filter: ModelUserFollowFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    userFollowings(
+      followerID: $followerID
+      followeeID: $followeeID
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        followerID
+        followeeID
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        owner
+      }
+      nextToken
+      startedAt
+    }
+  }
+`;
